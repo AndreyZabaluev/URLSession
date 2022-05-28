50a86998b17f0ecbb717981869ab3aeceba6d83d
AndreyZabaluev <102421313+AndreyZabaluev@users.noreply.github.com>
2022-05-29 00:01:48 +0300

    Add files via upload
    Приложение, запрашивает API поиска iTunes и загружает 30-секундные превью песен.

---
+++ URLSession Starter/HalfTunes/Model/Track.swift
@@ -0,0 +1,18 @@
+import Foundation.NSURL
+
+class Track {
+
+  let artist: String
+  let index: Int
+  let name: String
+  let previewURL: URL
+  
+  var downloaded = false
+
+  init(name: String, artist: String, previewURL: URL, index: Int) {
+    self.name = name
+    self.artist = artist
+    self.previewURL = previewURL
+    self.index = index
+  }
+}
